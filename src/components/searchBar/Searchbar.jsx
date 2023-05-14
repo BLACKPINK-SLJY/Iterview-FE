9a5c8f0aa33d423b600004a9fb064f23b2e630a6
seocylucky <seocylucky@github.com>
2023-05-14 23:41:46 +0900

    feat: 검색바 작업

---
+++ src/components/searchBar/Searchbar.jsx
@@ -0,0 +1,90 @@
+import React,{useState, useRef} from 'react'
+import { StyleSearchBar, MagnifierImg, SearchBoxStyle } from './style';
+import Magnifier from "../../assets/svg/magnifier.svg";
+// import { requestSearch } from '../../../apis/index';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar = () => {
+
+    const [search, setSearch] = useState("");
+    const [result, setResult] = useState([]);
+    const [isSuccess, setIsSuccess] = useState(true);
+    const navigate = useNavigate();
+    
+    console.log(search)
+    const input = useRef();
+
+
+    const searchQuery = (query) => {
+        if (query === "") return;
+        try {
+            // requestSearch(query)
+            //     .then((res) => {
+            //         // if (res.data === []) setIsSuccess(false);
+            //         // else setIsSuccess(true);
+
+            //         setResult(res.data);
+            //         console.log(search);
+            //         console.log(result);
+            //     })
+            //     .catch((e) => {
+            //         setResult([]);
+            //         setIsSuccess(false);
+            //     });
+        } catch (err) {
+            console.log(err);
+        } finally {
+        }
+    };
+
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    // useEffect(() => {
+    //     setResult();
+    //     setSearch();
+    // })
+
+    // const onClickSearchHandler = () => {
+    //     navigate("/search");
+    // }
+
+    return (
+        <>
+            <SearchBoxStyle>
+                <StyleSearchBar
+                    type="search"
+                    ref={input}
+                    value={search}
+                    onChange={onChangeSearch}
+                    onKeyDown={() => {
+                        if (window.event.keyCode === 13) {
+                            searchQuery(search);
+                            navigate("/search", {
+                                state: {
+                                    content: search,
+                                    searchResult: result,
+                                }
+                            });
+                            setSearch("");
+                            
+                        }
+                    }}
+                    onSubmit={(e)=>{
+                        searchQuery(search);
+                        setSearch(e.target.value);
+                        input.current.value = "";
+                    }}
+                    placeholder="검색어를 입력하세요">
+                </StyleSearchBar>
+                <MagnifierImg src={Magnifier}></MagnifierImg>
+            </SearchBoxStyle>
+            {search !== "" && isSuccess && result.length > 0 }
+            
+        </>
+    );
+};
+
+export default SearchBar
---
+++ src/components/searchBar/style.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const SearchBoxStyle = styled.div`
+    height: 40px;
+    position: relative;
+    
+    display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+
+`
+
+export const StyleSearchBar = styled.input`
+    width: 340px;
+    height: 40px;
+
+    background: #F5F5F7;
+    border-radius: 42px;
+    border: none;
+    padding-left: 22px;
+    font-weight: 300;
+    font-size: 15px;
+    line-height: 20px;
+    letter-spacing: 0.05em;
+    outline: none;
+    &:focus::placeholder {
+        color: transparent;
+        border: none;
+    }
+
+    box-sizing: border-box;
+`
+
+export const MagnifierImg = styled.img`
+    margin-right: 19px;
+    width: 22px;
+    height: 22px;
+    position: absolute;
+`
