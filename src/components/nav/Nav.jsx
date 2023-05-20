83e084e5743dc00e197dda7d34bac6313649c50b
seocylucky <seocylucky@github.com>
2023-05-20 20:18:24 +0900

    feat: nav dropdown 작업

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { Logos, NavLayout, SignButtonLayout, SignUpButton, LoginButton, SearchNickLayout } from './style'
 import Logo from "../../assets/svg/Logo.svg"
 import Logo2 from "../../assets/svg/Logo_2.svg"
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '../searchBar/Searchbar';
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import useDetectClose from './useDetectClose';
+import colors from '../../style/color';
 
 function Nav() {
+  const [selectIsOpen, selectRef, selectHandler] = useDetectClose(false)
+  const [myPageIsOpen, myPageRef, myPageHandler] = useDetectClose(false);
   const navigate = useNavigate();
 
   const onClickLogin = () => {
@@ -29,10 +33,28 @@ function Nav() {
         <Logos onClick={onClickHome}>
             <img src={Logo} alt="logo" style={{cursor: "pointer"}}/>
             <img src={Logo2} alt="iterview" style={{cursor: "pointer"}}/>
-            <DropdownSelect style={{marginLeft: "14px"}}>
-            <TextStyle>직무선택</TextStyle>
-            <FontAwesomeIcon icon={faAngleDown} style={{color:"white", paddingTop:"13px"}} />
-            </DropdownSelect>
+            <DropdownContainer>
+              <DropdownSelect style={{marginLeft: "14px"}} onClick={selectHandler} ref={selectRef}>
+              <TextStyle>직무선택</TextStyle>
+              <FontAwesomeIcon icon={faAngleDown} style={{color:"white", paddingTop:"13px"}} />
+              </DropdownSelect>
+              <DropDownMenu isDropped={selectIsOpen}>
+                <Ul>
+                  <li>
+                    <LinkWrapper href="#1-1">Frontend</LinkWrapper>
+                  </li>
+                  <li>
+                    <LinkWrapper href="#1-2">Backend</LinkWrapper>
+                  </li>
+                  <li>
+                    <LinkWrapper href="#1-4">Android</LinkWrapper>
+                  </li>
+                  <li>
+                    <LinkWrapper href="#1-5">ios</LinkWrapper>
+                  </li>
+                </Ul>
+              </DropDownMenu>
+              </DropdownContainer>
         </Logos>
         {/* <SignButtonLayout>
             <SignUpButton onClick={onClickSignup}>회원가입</SignUpButton>
@@ -40,9 +62,23 @@ function Nav() {
         </SignButtonLayout> */}
         <SearchNickLayout>
         <SearchBar />
-        <TextStyle>유민진<SpanStyle>님</SpanStyle>
-        <FontAwesomeIcon icon={faAngleDown} style={{color:"white", marginLeft: "7px"}} />
-        </TextStyle>
+        <DropdownContainer>
+          <DropdownSelect onClick={myPageHandler} ref={myPageRef}>
+            <TextStyle>유민진<SpanStyle>님</SpanStyle>
+            <FontAwesomeIcon icon={faAngleDown} style={{color:"white", marginLeft: "7px"}} />
+            </TextStyle>      
+          </DropdownSelect>
+          <DropDownMenu2 isDropped={myPageIsOpen}>
+            <Ul>
+              <li>
+              <LinkWrapper href="#1-1">My Page</LinkWrapper>
+              </li>
+              <li>
+              <LinkWrapper href="#1-2">Log Out</LinkWrapper>
+              </li>
+            </Ul>
+          </DropDownMenu2>
+        </DropdownContainer>
         </SearchNickLayout>
     </NavLayout>
     </>
@@ -69,3 +105,79 @@ const DropdownSelect = styled.div`
   gap: 6px;
   cursor: pointer;
 `
+const DropDownMenu = styled.div`
+  background-color: ${colors.black_100};
+  position: absolute;
+  top: 69px;
+  left: 50%;
+  width: 258px;
+  height: 235px;
+  text-align: center;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  opacity: 0;
+  visibility: hidden;
+  transform: translate(-50%, -20px);
+  transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+  z-index: 9;
+  border-radius: 0px 0px 10px 10px;
+
+  ${({ isDropped }) =>
+    isDropped &&
+    css`
+      opacity: 1;
+      visibility: visible;
+      transform: translate(-50%, 0);
+      left: 50%;
+    `};
+`
+const DropDownMenu2 = styled.div`
+  background-color: ${colors.black_100};
+  position: absolute;
+  top: 69px;
+  left: 50%;
+  width: 258px;
+  height: 122px;
+  text-align: center;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  opacity: 0;
+  visibility: hidden;
+  transform: translate(-50%, -20px);
+  transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+  z-index: 9;
+  border-radius: 0px 0px 10px 10px;
+
+  ${({ isDropped }) =>
+    isDropped &&
+    css`
+      opacity: 1;
+      visibility: visible;
+      transform: translate(-50%, 0);
+      left: 50%;
+    `};
+`
+const LinkWrapper = styled.a`
+  font-weight: 500;
+  font-size: 18.4932px;
+  line-height: 27px;
+  letter-spacing: 0.05em;
+  text-decoration: none;
+  color: white;
+`;
+const Ul = styled.ul`
+  & > li {
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const DropdownContainer = styled.div`
+  position: relative;
+  text-align: center;
+`;
