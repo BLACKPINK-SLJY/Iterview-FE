ba2f08cfbf7edaebd7fd5f2b1d6a804e03e2ef58
seocylucky <seocylucky@github.com>
2023-05-22 15:31:05 +0900

    feat: 테스트용 데이터 파일

---
+++ src/data/data.jsx
@@ -0,0 +1,48 @@
+const data = [
+    {
+        id: 1,
+        category: "Frontend",
+        questions : [
+            { 
+                question : "질문어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌쩌구어쩌구어쩌쩌구어쩌구어쩌쩌구어쩌구어쩌쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+                level: "Hard",
+                keywords: ["React", "JavaScript", "Spring"]
+            }
+        ]
+    },
+    {
+        id: 2,
+        category: "Backend",
+        questions : [
+            { 
+                question : "질문어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+                level: "Normal",
+                keywords: ["React", "JavaScript", "Spring"]
+            }
+        ]
+    },
+    {
+        id: 3,
+        category: "Android",
+        questions : [
+            { 
+                question : "질문어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+                level: "Easy",
+                keywords: ["React", "JavaScript", "Spring"]
+            }
+        ]
+    },
+    {
+        id: 4,
+        category: "ios",
+        questions : [
+            { 
+                question : "질문어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+                level: "Hard",
+                keywords: ["React", "JavaScript", "Spring"]
+            }
+        ]
+    }
+]
+
+export default data;
