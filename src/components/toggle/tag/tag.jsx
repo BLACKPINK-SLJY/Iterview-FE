a54e909320b7d05492d77d2ed31b7a93753b964e
seocylucky <seocylucky@github.com>
2023-05-27 22:10:40 +0900

    fix: tag 디자인 수정

--- src/components/toggle/tag/tag.jsx
+++ src/components/toggle/tag/tag.jsx
@@ -14,12 +14,17 @@ function Tag() {
 export default Tag
 
 const Container = styled.div`
-    box-sizing: border-box;
     width: fit-content;
     height: 34px;
+    padding-top: 10px;
     text-align: center;
     border-radius: 55.8058px;
     border: 0.707781px solid black;
-    color: black;
-    padding: 6.8px 12px;
+    color: ${colors.black_80};
+    font-weight: 500;
+    font-size: 18.1612px;
+    line-height: 16px;
+    padding-left: 12px;
+    padding-right: 12px;
+    padding-top: 10px;
 `
